3867896a887e33331e008e4df4fdf942b89b0b87
Domagoj Sucic <domagoj.sucic1@gmail.com>
2023-02-08 20:17:05 -0700

    Created Controller Month Vent object

---
+++ src/graphql/domain/controller_month_vent.rs
@@ -0,0 +1,46 @@
+use crate::graphql::{
+    context::ContextExt,
+    dataloaders::{controller_loader::ControllerLoader, user_loader::UserLoader},
+    domain::{Controller, User},
+};
+use async_graphql::{dataloader::DataLoader, ComplexObject, Context, Error, SimpleObject};
+use chrono::{NaiveDate, NaiveDateTime};
+use sqlx::FromRow;
+use uuid::Uuid;
+
+#[derive(SimpleObject, Clone, FromRow, Debug)]
+#[graphql(complex)]
+pub struct ControllerMonthVent {
+    pub id: Uuid,
+    pub month: NaiveDate,
+    pub volume: f64,
+    pub controller_id: Uuid,
+    pub created_by_id: Uuid,
+    pub created_at: NaiveDateTime,
+    pub updated_by_id: Uuid,
+    pub updated_at: NaiveDateTime,
+}
+
+#[ComplexObject]
+impl ControllerMonthVent {
+    async fn created_by(&self, ctx: &Context<'_>) -> Result<Option<User>, Error> {
+        let loader = ctx.get_loader::<DataLoader<UserLoader>>();
+        let created_by = loader.load_one(self.created_by_id).await;
+
+        created_by
+    }
+
+    async fn updated_by(&self, ctx: &Context<'_>) -> Result<Option<User>, Error> {
+        let loader = ctx.get_loader::<DataLoader<UserLoader>>();
+        let updated_by = loader.load_one(self.updated_by_id).await;
+
+        updated_by
+    }
+
+    async fn controller(&self, ctx: &Context<'_>) -> Result<Option<Controller>, Error> {
+        let loader = ctx.get_loader::<DataLoader<ControllerLoader>>();
+        let controller = loader.load_one(self.controller_id).await;
+
+        controller
+    }
+}
--- src/graphql/domain/mod.rs
+++ src/graphql/domain/mod.rs
@@ -5,6 +5,7 @@ mod controller_application;
 mod controller_change;
 mod controller_manufacturer;
 mod controller_month_hours;
+mod controller_month_vent;
 mod facility;
 mod user;
 
@@ -15,5 +16,6 @@ pub use controller_application::*;
 pub use controller_change::*;
 pub use controller_manufacturer::*;
 pub use controller_month_hours::*;
+pub use controller_month_vent::*;
 pub use facility::*;
 pub use user::*;
