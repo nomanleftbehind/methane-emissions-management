1ad55249f8e27d2faffd60b24c4ba70b333a9104
Doma Sucic <domagoj.sucic1@gmail.com>
2023-04-27 17:19:50 -0600

    Modified compressor seal calculation query to take controlled characterization into account.

--- server/src/graphql/sql/month_methane_emission.rs
+++ server/src/graphql/sql/month_methane_emission.rs
@@ -45,16 +45,16 @@ pub async fn insert_month_methane_emissions(
     .fetch_all(pool)
     .await?;
 
-    // let mut compressor_seal_month_methane_emissions_calculated = query_file_as!(
-    //     MonthMethaneEmissionCalculated,
-    //     "src/graphql/sql/statements/compressor_seal_month_methane_emission_calculate.sql",
-    //     from_month,
-    //     to_month,
-    //     c1,
-    //     co2
-    // )
-    // .fetch_all(pool)
-    // .await?;
+    let mut compressor_seal_month_methane_emissions_calculated = query_file_as!(
+        MonthMethaneEmissionCalculated,
+        "src/graphql/sql/statements/compressor_seal_month_methane_emission_calculate.sql",
+        from_month,
+        to_month,
+        c1,
+        co2
+    )
+    .fetch_all(pool)
+    .await?;
 
     // let mut compressor_blowdown_month_methane_emissions_calculated = query_file_as!(
     //     MonthMethaneEmissionCalculated,
@@ -67,8 +67,8 @@ pub async fn insert_month_methane_emissions(
     // .fetch_all(pool)
     // .await?;
 
-    // pneumatic_device_month_methane_emissions_calculated
-    //     .append(&mut compressor_seal_month_methane_emissions_calculated);
+    pneumatic_device_month_methane_emissions_calculated
+        .append(&mut compressor_seal_month_methane_emissions_calculated);
 
     // pneumatic_device_month_methane_emissions_calculated
     //     .append(&mut compressor_blowdown_month_methane_emissions_calculated);
--- server/src/graphql/sql/statements/compressor_seal_month_methane_emission_calculate.sql
+++ server/src/graphql/sql/statements/compressor_seal_month_methane_emission_calculate.sql
@@ -7,13 +7,23 @@ WITH allocate_month as (
 SELECT
 	csmme.facility_id,
 	csmme.site_id as "site_id!",
-	'compressor_seal'::methane_emission_source as "source!: _",
-	csmme.id as "source_id!",
-	'ROUTINE'::methane_emission_category as "category!: _",
+	'compressor_seal'::methane_emission_source_table as "source_table!: _",
+	csmme.id as "source_table_id!",
+	CASE
+		WHEN csmme.controlled_characterization = 'UNCONTROLLED' THEN 'ROUTINE'::methane_emission_category
+		ELSE 'NONROUTINE'::methane_emission_category
+	END as "category!: _",
+	CASE
+		WHEN csmme.controlled_characterization = 'UNCONTROLLED' THEN CASE
+			WHEN cpr.type IN ('RECIPROCATING', 'CENTRIFUGAL') THEN 'COMPRESSOR_SEAL'::methane_emission_source
+			ELSE 'DEFINED_VENT_GAS'::methane_emission_source
+		END
+		ELSE 'PLANNED'::methane_emission_source
+	END as "source!: _",
 	csmme.month_beginning as "month!",
-	SUM(csmme.gas_volume * csmme.percent) as "gas_volume!",
-	SUM(csmme.gas_volume * csmme.c1 * csmme.percent) as "c1_volume!",
-	SUM(csmme.gas_volume * csmme.co2 * csmme.percent) as "co2_volume!"
+	csmme.gas_volume as "gas_volume!",
+	csmme.c1_volume as "c1_volume!",
+	csmme.co2_volume as "co2_volume!"
 FROM
 	(
 		SELECT
@@ -21,37 +31,50 @@ FROM
 			csmme.site_id,
 			csmme.id,
 			csmme.month_beginning,
-			csmme.gas_volume,
-			csmme.c1,
-			csmme.co2,
-			EXTRACT(
-				DAY
-				FROM
-					(
-						csmme.to_date + INTERVAL '1 day' - csmme.from_date
-					)
-			) / csmme.days_in_period as percent
+			csmme.controlled_characterization,
+			SUM(csmme.gas_volume * csmme.percent) as gas_volume,
+			SUM(csmme.gas_volume * csmme.c1 * csmme.percent) as c1_volume,
+			SUM(csmme.gas_volume * csmme.co2 * csmme.percent) as co2_volume
 		FROM
 			(
 				SELECT
-					s.facility_id,
+					csmme.facility_id,
 					csmme.site_id,
 					csmme.id,
 					csmme.month_beginning,
+					csmme.from_date,
+					csmme.to_date,
 					csmme.gas_volume,
-					COALESCE(ga.c1, $3) as c1,
-					COALESCE(ga.co2, $4) as co2,
-					GREATEST(ga.from_date, csmme.from_date) as from_date,
-					LEAST(ga.to_date, csmme.to_date) as to_date,
+					csmme.c1,
+					csmme.co2,
+					csmme.controlled_characterization,
 					EXTRACT(
 						DAY
 						FROM
-							(csmme.to_date + INTERVAL '1 day' - csmme.from_date)
-					) as days_in_period
+							(
+								csmme.to_date + INTERVAL '1 day' - csmme.from_date
+							)
+					) / csmme.days_in_period as percent
 				FROM
 					(
 						SELECT
-							csmme.*
+							s.facility_id,
+							csmme.site_id,
+							csmme.id,
+							csmme.month_beginning,
+							csmme.gas_volume,
+							COALESCE(ga.c1, $3) as c1,
+							COALESCE(ga.co2, $4) as co2,
+							csmme.controlled_characterization,
+							GREATEST(ga.from_date, csmme.from_date) as from_date,
+							LEAST(ga.to_date, csmme.to_date) as to_date,
+							EXTRACT(
+								DAY
+								FROM
+									(
+										csmme.to_date + INTERVAL '1 day' - csmme.from_date
+									)
+							) as days_in_period
 						FROM
 							(
 								SELECT
@@ -60,7 +83,8 @@ FROM
 									csmme.month_beginning,
 									csmme.from_date,
 									csmme.to_date,
-									csmme.hours_on * csmme.rate * csmme.percent as gas_volume
+									csmme.gas_volume * csmme.percent as gas_volume,
+									csmme.controlled_characterization
 								FROM
 									(
 										SELECT
@@ -69,71 +93,175 @@ FROM
 											csmme.month_beginning,
 											csmme.from_date,
 											csmme.to_date,
-											csmme.hours_on,
-											csmme.rate,
+											csmme.gas_volume,
+											csmme.controlled_characterization,
 											EXTRACT(
 												DAY
 												FROM
-													(csmme.to_date + INTERVAL '1 day' - csmme.from_date)
-											) / csmme.days_in_month as percent
+													(
+														csmme.to_date + INTERVAL '1 day' - csmme.from_date
+													)
+											) / csmme.days_in_period as percent
 										FROM
 											(
 												SELECT
-													cmpr.id,
-													cmpr.site_id,
-													cmpr.month_beginning,
-													GREATEST(cst.from_date, cmpr.month_beginning) as from_date,
-													LEAST(
-														cst.to_date,
-														cmpr.month_beginning + INTERVAL '1 month - 1 day'
-													) as to_date,
-													DATE_PART(
-														'days',
-														cmpr.month_beginning + INTERVAL '1 month - 1 day'
-													) as days_in_month,
-													COALESCE(cmh.pressurized_hours, 0) hours_on,
-													COALESCE(cst.rate, 0) rate
+													csmme.id,
+													csmme.site_id,
+													csmme.month_beginning,
+													csmme.gas_volume,
+													COALESCE(ccc.controlled_characterization, 'UNCONTROLLED') as controlled_characterization,
+													GREATEST(ccc.from_date, csmme.from_date) as from_date,
+													LEAST(ccc.to_date, csmme.to_date) as to_date,
+													EXTRACT(
+														DAY
+														FROM
+															(
+																csmme.to_date + INTERVAL '1 day' - csmme.from_date
+															)
+													) as days_in_period
 												FROM
 													(
 														SELECT
-															cs.id,
-															cpr.site_id,
-															am.month_beginning
+															csmme.*
 														FROM
-															compressor_seal cs -- Compressor seal and compressor share same id, however compressor is joined here because it contains site_id, install_date and remove_date columns, which are used subsequently.
-															LEFT OUTER JOIN compressor cpr ON cpr.id = cs.id -- Despite sharing same id, existence of compressor doesn't guarantee existence of compressor seal, hence why joining allocation months on compressor is not done here.
-															INNER JOIN allocate_month am ON am.month_beginning BETWEEN DATE_TRUNC('month', cpr.install_date)
-															AND COALESCE(cpr.remove_date, CURRENT_DATE)
-															AND (cs.id, am.month_beginning) NOT IN (
+															(
 																SELECT
-																	csmmeo.compressor_seal_id,
-																	csmmeo.month
+																	csmme.id,
+																	csmme.site_id,
+																	csmme.month_beginning,
+																	csmme.from_date,
+																	csmme.to_date,
+																	csmme.hours_on * csmme.rate * csmme.percent as gas_volume
 																FROM
-																	compressor_seal_month_methane_emission_override csmmeo
-																	INNER JOIN allocate_month am ON am.month_beginning = csmmeo.month
-															)
-													) cmpr
-													LEFT OUTER JOIN compressor_month_hours cmh ON cmh.compressor_id = cmpr.id
-													AND cmh.month = cmpr.month_beginning
+																	(
+																		SELECT
+																			csmme.id,
+																			csmme.site_id,
+																			csmme.month_beginning,
+																			csmme.from_date,
+																			csmme.to_date,
+																			csmme.hours_on,
+																			csmme.rate,
+																			EXTRACT(
+																				DAY
+																				FROM
+																					(
+																						csmme.to_date + INTERVAL '1 day' - csmme.from_date
+																					)
+																			) / csmme.days_in_month as percent
+																		FROM
+																			(
+																				SELECT
+																					cmpr.id,
+																					cmpr.site_id,
+																					cmpr.month_beginning,
+																					GREATEST(cst.from_date, cmpr.month_beginning) as from_date,
+																					LEAST(
+																						cst.to_date,
+																						cmpr.month_beginning + INTERVAL '1 month - 1 day'
+																					) as to_date,
+																					DATE_PART(
+																						'days',
+																						cmpr.month_beginning + INTERVAL '1 month - 1 day'
+																					) as days_in_month,
+																					COALESCE(cmh.pressurized_hours, 0) hours_on,
+																					COALESCE(cst.rate, 0) rate
+																				FROM
+																					(
+																						SELECT
+																							cs.id,
+																							cpr.site_id,
+																							am.month_beginning
+																						FROM
+																							compressor_seal cs -- Compressor seal and compressor share same id, however compressor is joined here because it contains site_id, install_date and remove_date columns, which are used subsequently.
+																							LEFT OUTER JOIN compressor cpr ON cpr.id = cs.id -- Despite sharing same id, existence of compressor doesn't guarantee existence of compressor seal, hence why joining allocation months on compressor is not done here.
+																							INNER JOIN allocate_month am ON am.month_beginning BETWEEN DATE_TRUNC('month', cpr.install_date)
+																							AND COALESCE(cpr.remove_date, CURRENT_DATE)
+																							AND (cs.id, am.month_beginning) NOT IN (
+																								SELECT
+																									csmmeo.compressor_seal_id,
+																									csmmeo.month
+																								FROM
+																									compressor_seal_month_methane_emission_override csmmeo
+																									INNER JOIN allocate_month am ON am.month_beginning = csmmeo.month
+																							)
+																					) cmpr
+																					LEFT OUTER JOIN compressor_month_hours cmh ON cmh.compressor_id = cmpr.id
+																					AND cmh.month = cmpr.month_beginning
+																					LEFT OUTER JOIN (
+																						SELECT
+																							compressor_seal_id,
+																							DATE_TRUNC('month', date) as month_join_beginning,
+																							DATE_TRUNC(
+																								'month',
+																								COALESCE(
+																									LEAD(date) OVER (
+																										PARTITION BY compressor_seal_id
+																										ORDER BY
+																											date
+																									) - INTERVAL '1 day',
+																									CURRENT_DATE
+																								)
+																							) + INTERVAL '1 month - 1 day' as month_join_end,
+																							-- If first seal test, from_date has to be first of the month because there is no carryover from previous test.
+																							CASE
+																								WHEN ROW_NUMBER() OVER (
+																									PARTITION BY compressor_seal_id
+																									ORDER BY
+																										date
+																								) = 1 THEN DATE_TRUNC('month', date)::date
+																								ELSE date
+																							END as from_date,
+																							COALESCE(
+																								LEAD(date) OVER (
+																									PARTITION BY compressor_seal_id
+																									ORDER BY
+																										date
+																								) - INTERVAL '1 day',
+																								CURRENT_DATE
+																							) as to_date,
+																							rate
+																						FROM
+																							compressor_seal_test
+																					) cst ON cst.compressor_seal_id = cmpr.id
+																					AND cmpr.month_beginning BETWEEN cst.month_join_beginning
+																					AND cst.month_join_end
+																			) csmme
+																	) csmme
+															) csmme
+														UNION
+														ALL
+														SELECT
+															csmme.*
+														FROM
+															(
+																SELECT
+																	cpr.id,
+																	cpr.site_id,
+																	csmmeo.month as month_beginning,
+																	csmmeo.month as from_date,
+																	csmmeo.month + INTERVAL '1 month - 1 day' as to_date,
+																	csmmeo.gas_volume
+																FROM
+																	compressor_seal_month_methane_emission_override csmmeo -- Unlike earlier example where override was joined on compressor seal, where it could have seemingly been joined on compressor, here it is joined on compressor because existence of override entry guarantees existence of compressor seal.
+																	INNER JOIN compressor cpr ON cpr.id = csmmeo.compressor_seal_id
+																	AND csmmeo.month BETWEEN DATE_TRUNC('month', cpr.install_date)
+																	AND COALESCE(cpr.remove_date, CURRENT_DATE)
+																	AND csmmeo.month IN (
+																		SELECT
+																			month_beginning
+																		FROM
+																			allocate_month
+																	)
+															) csmme
+													) csmme
 													LEFT OUTER JOIN (
 														SELECT
-															compressor_seal_id,
-															DATE_TRUNC('month', date) as month_join_beginning,
-															DATE_TRUNC(
-																'month',
-																COALESCE(
-																	LEAD(date) OVER (
-																		PARTITION BY compressor_seal_id
-																		ORDER BY
-																			date
-																	) - INTERVAL '1 day',
-																	CURRENT_DATE
-																)
-															) + INTERVAL '1 month - 1 day' as month_join_end,
-															-- If first seal test, from_date has to be first of the month because there is no carryover from previous test.
+															compressor_id,
+															-- If first controlled characterization, from_date has to be first of the month because there is no carryover from previous controlled characterization.
 															CASE
 																WHEN ROW_NUMBER() OVER (
-																	PARTITION BY compressor_seal_id
+																	PARTITION BY compressor_id
 																	ORDER BY
 																		date
 																) = 1 THEN DATE_TRUNC('month', date)::date
@@ -141,71 +269,56 @@ FROM
 															END as from_date,
 															COALESCE(
 																LEAD(date) OVER (
-																	PARTITION BY compressor_seal_id
+																	PARTITION BY compressor_id
 																	ORDER BY
 																		date
 																) - INTERVAL '1 day',
 																CURRENT_DATE
 															) as to_date,
-															rate
+															controlled_characterization
 														FROM
-															compressor_seal_test
-													) cst ON cst.compressor_seal_id = cmpr.id
-													AND cmpr.month_beginning BETWEEN cst.month_join_beginning
-													AND cst.month_join_end
+															compressor_controlled_characterization
+													) ccc ON ccc.compressor_id = csmme.id
+													AND ccc.from_date <= csmme.to_date
+													AND ccc.to_date >= csmme.from_date
 											) csmme
 									) csmme
 							) csmme
-						UNION
-						ALL
-						SELECT
-							csmme.*
-						FROM
-							(
+							LEFT OUTER JOIN site s ON s.id = csmme.site_id
+							LEFT OUTER JOIN (
 								SELECT
-									cpr.id,
-									cpr.site_id,
-									csmmeo.month as month_beginning,
-									csmmeo.month as from_date,
-									csmmeo.month + INTERVAL '1 month - 1 day' as to_date,
-									csmmeo.gas_volume
+									facility_id,
+									-- If first gas analysis, from_date has to be first of the month because there is no carryover from previous analysis.
+									CASE
+										WHEN ROW_NUMBER() OVER (
+											PARTITION BY facility_id
+											ORDER BY
+												date
+										) = 1 THEN DATE_TRUNC('month', date)::date
+										ELSE date
+									END as from_date,
+									COALESCE(
+										LEAD(date) OVER (
+											PARTITION BY facility_id
+											ORDER BY
+												date
+										) - INTERVAL '1 day',
+										CURRENT_DATE
+									) as to_date,
+									c1,
+									co2
 								FROM
-									compressor_seal_month_methane_emission_override csmmeo -- Unlike earlier example where override was joined on compressor seal, where it could have seemingly been joined on compressor, here it is joined on compressor because existence of override entry guarantees existence of compressor seal.
-									INNER JOIN compressor cpr ON cpr.id = csmmeo.compressor_seal_id
-									AND csmmeo.month BETWEEN DATE_TRUNC('month', cpr.install_date)
-									AND COALESCE(cpr.remove_date, CURRENT_DATE)
-									AND csmmeo.month IN (
-										SELECT
-											month_beginning
-										FROM
-											allocate_month
-									)
-							) csmme
+									gas_analysis
+							) ga ON ga.facility_id = s.facility_id
+							AND ga.from_date <= csmme.to_date
+							AND ga.to_date >= csmme.from_date
 					) csmme
-					LEFT OUTER JOIN site s ON s.id = csmme.site_id
-					LEFT OUTER JOIN (
-						SELECT
-							facility_id,
-							date as from_date,
-							COALESCE(
-								LEAD(date) OVER (
-									PARTITION BY facility_id
-									ORDER BY
-										date
-								) - INTERVAL '1 day',
-								CURRENT_DATE
-							) as to_date,
-							c1,
-							co2
-						FROM
-							gas_analysis
-					) ga ON ga.facility_id = s.facility_id
-					AND ga.from_date <= csmme.to_date
-					AND ga.to_date >= csmme.from_date
 			) csmme
+		GROUP BY
+			csmme.facility_id,
+			csmme.site_id,
+			csmme.id,
+			csmme.month_beginning,
+			csmme.controlled_characterization
 	) csmme
-GROUP BY
-	csmme.facility_id,
-	csmme.site_id,
-	csmme.id,
-	csmme.month_beginning
+	LEFT OUTER JOIN compressor cpr ON cpr.id = csmme.id
